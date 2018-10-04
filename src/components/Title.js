ed9dee25d76b29fe2883aa55ca37b8840bed0506
Kamil Szczypior <kszczypior1@gmail.com>
2018-10-04 11:22:10 +0200

    Fix babel-core bug

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Title = props => 
     <div className="title">
-        <h1>React ToDo App</h1>
+        <h1>Webpack config 2</h1>
         <ol>
             {props.data.map(result => <li key={result.id}>{result.text}</li>)} 
         </ol>
