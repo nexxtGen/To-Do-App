ce85d9de7d75e07184a087ccf568dae7733619a6
Kamil Szczypior <kszczypior1@gmail.com>
2018-10-10 23:43:30 +0200

    Add all scripts

---
+++ src/components/TodoForm.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+class TodoForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            valueForm: ''
+        };        
+    }
+
+    submit(event) {
+        this.props.addItemFunc(this.state.valueForm);
+        event.preventDefault(); //Zatrzymuje przeładowanie App
+    }
+
+    onChangeForm(event) {
+        const val = event.currentTarget.value; 
+        this.setState({ valueForm: val });
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.submit.bind(this)}>
+                    <label>
+                        <p>Add new Item:</p>
+                        <input type="text" value={this.state.value} onChange={this.onChangeForm.bind(this)}></input>
+                    </label>
+                    <input type="submit" value="Submit"></input>
+                </form>
+            </div>
+        )
+    }
+};
+/* // Komponent funkcyjny który trochę źle działa . // Źle działa bo chyba nie ma w addTodo event.preventdefault()
+const TodoForm = props => (
+    <div>
+        <form onSubmit={() => props.addTodo(props.value)}>
+            <label>
+                <p>Add new Item:</p>
+                <input type="text" value={props.value} onChange={props.onChange}></input>
+            </label>
+            <input type="submit" value="Submit"></input>
+        </form>
+    </div>
+)
+*/
+export default TodoForm;
--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,7 @@ import style from './App.css';
 import Title from '../components/Title';
 import TodoList from '../components/TodoList';
 import TodoFilter from '../components/TodoFilter';
+import TodoForm from '../components/TodoForm';
 import { hot } from 'react-hot-loader';
 
 class App extends React.Component {
@@ -22,26 +23,34 @@ class App extends React.Component {
                 }
                 
             ],
-            filter: ''
+            filter: '',
+            //formValue: '' //Jest potrzebne w przypadku uzycia komponentu funkcyjnego TodoForm
         }; // #średniklivesmatter        
     }
 
     //funkcje/metody
-    addTodo(val) { //metoda dodaje nowe elementy do kolekcji
+    addTodo(val) { //metoda dodaje nowe elementy do kolekcji        
         const todo = {
             text: val,
             id: uuid.v4(), // biblioteka uuid (npm install --save uuid) // Dlaczego dali tu przecinek?            
         }; // #średniklivesmatter
         const data = [...this.state.data, todo];
-        this.setState({data}); // TO to samo co {data: data}
+        this.setState({data}); // TO to samo co {data: data}        
     }
+    /* metoda do komponentu funkcyjnego, który nie działa do konca. Trzeba zatrzymac przekazywanie w góre drzewa DOM
+    onChange(event) {
+        const formValue = event.currentTarget.value;
 
+        this.setState({ formValue });
+    }
+    */
     //Usuwanie rzeczy do zrobienia
     removeTodo(id) {
         const remainder = this.state.data.filter(todo => todo.id !== id);
 
         this.setState({data: remainder});
     }
+
     // Filtrowanie listy
     onFilterChange(event) {
         const value = event.currentTarget.value;
@@ -53,6 +62,9 @@ class App extends React.Component {
         return (
             <div className={style.TodoApp}>
                 <Title data={this.state.data}/>
+                <TodoForm addItemFunc={this.addTodo.bind(this)}/>
+                
+
                 <TodoFilter filter={this.state.filter} onFilterChange={this.onFilterChange.bind(this)}/>
                 <TodoList data={this.state.data} remove={this.removeTodo.bind(this)} filter={this.state.filter}/>
             </div>
@@ -61,3 +73,5 @@ class App extends React.Component {
 }
 
 export default hot(module)(App);
+// Komponent funkcyjny który trochę źle działa
+//<TodoForm value={this.state.formValue} addTodo={this.addTodo.bind(this)} onChange={this.onChange.bind(this)}/>
